114325b45c034b3a541652528fde2ba749332a0e
richard <xrmxrm@gmail.com>
2024-02-25 12:30:23 -0800

    Implement left/right movement

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -14,6 +15,24 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Use float for better precision in horizontal position
+        # Attributes of rect are integers, so make attribute of ship
+        self.x = float(self.rect.x)
+
+        # Movement flags (False = not moving)
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update ship's position and keep it on screen"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #Update rect x from ship's x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
